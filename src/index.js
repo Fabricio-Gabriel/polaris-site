9db60755a7e6865053dd9de87896b2a5da7b3ae8
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-04-09 13:22:19 -0300

    adiciona home

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 let a1 = document.querySelector('.a1');
+let a2 = document.querySelector('.a2');
 
 if (window.location.href.includes("portfolio.html")) {
     a1.classList.add("addContent");
 } else {
     a1.classList.remove("addContent");
+}
+
+if (window.location.href.includes("index.html")) {
+    a2.classList.add("addContent");
+} else {
+    a2.classList.remove("addContent");
 }
