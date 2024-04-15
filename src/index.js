7343d36d021a4b0b4d4129f11cb9541698adb9d6
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-04-15 13:52:00 -0300

    organizar classes e js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-let a1 = document.querySelector('.a1');
-let a2 = document.querySelector('.a2');
+let portfolio = document.querySelector('.port');
+let index = document.querySelector('.index');
+let sobre = document.querySelector('.sobre');
 
-if (window.location.href.includes("portfolio.html")) {
-    a1.classList.add("addContent");
-} else {
-    a1.classList.remove("addContent");
-}
+const viewPage = (pagina, el) => {
+    if (window.location.href.includes(pagina)) {
+        el.classList.add("addContent");
+    } else {
+        el.classList.remove("addContent");
+    }
+};
 
-if (window.location.href.includes("index.html")) {
-    a2.classList.add("addContent");
-} else {
-    a2.classList.remove("addContent");
-}
+viewPage("portfolio.html", portfolio);
+viewPage("index.html", index);
+viewPage("sobre-nos.html", sobre);
+
+
+// if (window.location.href.includes("portfolio.html")) {
+//     portfolio.classList.add("addContent");
+// } else {
+//     portfolio.classList.remove("addContent");
+// }
+
+// if (window.location.href.includes("index.html")) {
+//     index.classList.add("addContent");
+// } else {
+//     index.classList.remove("addContent");
+// }
+
+// if (window.location.href.includes('sobre-nos.html')) {
+//     sobre.classList.add('addContent');
+// } else {
+//     sobre.classList.add
+// }
